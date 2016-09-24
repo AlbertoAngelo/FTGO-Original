0167bb472a67e3e50f671dadddedb3afce8b2d52
Alberto <Alberto@DESKTOP-UCAT7S6>
2016-09-24 16:56:39 -0300

    Novo Projeto

---
+++ foodtruck/Controllers/Index_SemLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class Index_SemLoginController : Controller
+    {
+        // GET: Index_SemLogin
+        public ActionResult Index_SemLogin()
+        {
+            return View();
+        }
+    }
+}
