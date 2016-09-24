96bda2a5389ed926e9dcdb9e480e28d52843d81b
WilliamFelipe34 <williamfelipe34@gmail.com>
2016-09-24 17:48:46 -0300

    Avaliação FT feita

--- foodtruck/Controllers/Avaliação_SemLoginController.cs
+++ foodtruck/Controllers/Avaliação_SemLoginController.cs
@@ -9,7 +9,7 @@ namespace FoodTruck.Controllers
     public class Avaliação_SemLoginController : Controller
     {
         // GET: Avaliação_SemLogin
-        public ActionResult Avaliação_SemLogin()
+        public ActionResult AvaliarFT_SL()
         {
             return View();
         }
