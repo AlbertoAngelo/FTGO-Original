1cc7cd9217c34d2a653e4b91644beca40481decd
WilliamFelipe34 <williamfelipe34@gmail.com>
2016-09-24 17:32:38 -0300

    Commit de tudo

---
+++ foodtruck/Controllers/Avaliação_SemLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class Avaliação_SemLoginController : Controller
+    {
+        // GET: Avaliação_SemLogin
+        public ActionResult Avaliação_SemLogin()
+        {
+            return View();
+        }
+    }
+}
