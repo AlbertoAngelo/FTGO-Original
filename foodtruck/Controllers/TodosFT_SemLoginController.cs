b3de808643849136d323dbfaf446e71032503a97
WilliamFelipe34 <williamfelipe34@gmail.com>
2016-09-28 21:35:41 -0300

    Criação da tela de todos os FT

---
+++ foodtruck/Controllers/TodosFT_SemLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class TodosFT_SemLoginController : Controller
+    {
+        // GET: TodosFT_SemLogin
+        public ActionResult TodosFT_SemLogin()
+        {
+            return View();
+        }
+    }
+}
