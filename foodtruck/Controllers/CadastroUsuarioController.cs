facc376c1dd15bc94b2180e615e576fad587b7eb
alberto angelo <alberto-angelo@hotmail.com>
2016-10-03 22:45:51 -0300

    Criada tela Cadastro de usuario

---
+++ foodtruck/Controllers/CadastroUsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class CadastroUsuarioController : Controller
+    {
+        // GET: CadastroUsuario
+        public ActionResult CadastroUsuario()
+        {
+            return View();
+        }
+    }
+}
