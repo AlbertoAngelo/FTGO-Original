ea912f11602fdb858cf0a49587c483b787e6c675
WilliamFelipe34 <williamfelipe34@gmail.com>
2016-10-04 23:30:16 -0300

    Inicio da criação do layout com login

---
+++ foodtruck/Controllers/Index_ComLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class Index_ComLoginController : Controller
+    {
+        // GET: Index_ComLogin
+        public ActionResult Index_ComLogin()
+        {
+            return View();
+        }
+    }
+}
