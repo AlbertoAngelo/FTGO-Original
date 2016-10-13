5b062e8a2ecd41359587621e69e467884a7a62a1
WilliamFelipe34 <williamfelipe34@gmail.com>
2016-10-13 15:32:02 -0300

    Coisa pra KRL

---
+++ foodtruck/Controllers/ProcurarFT_SemLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class ProcurarFT_SemLoginController : Controller
+    {
+        // GET: ProcurarFT_SemLogin
+        public ActionResult ProcurarFT_SemLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ foodtruck/Controllers/TodosFT_ComLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class TodosFT_ComLoginController : Controller
+    {
+        // GET: TodosFT_ComLogin
+        public ActionResult TodosFT_ComLogin()
+        {
+            return View();
+        }
+    }
+}
