fd1fcb63fcd5bc7ff6a8056ed8b5137441d67564
WilliamFelipe34 <williamfelipe34@gmail.com>
2016-10-18 21:17:40 -0200

    Criação da tela do dono do FT

---
+++ foodtruck/Controllers/DonoFtController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class DonoFtController : Controller
+    {
+        // GET: DonoFt
+        public ActionResult DonoFT()
+        {
+            return View();
+        }
+    }
+}
