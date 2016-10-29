116a5e7794f2946852276bf681df7f9b13c93057
WilliamFelipe34 <williamfelipe34@gmail.com>
2016-10-29 16:32:14 -0200

    Tela de confirmação de cadastro do FT

---
+++ foodtruck/Controllers/ConfirmarCadFTController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class ConfirmarCadFTController : Controller
+    {
+        // GET: ConfirmarCadFT
+        public ActionResult ConfirmarCad_FT()
+        {
+            return View();
+        }
+    }
+}
