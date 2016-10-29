43e4ef6992643d7881087799801f2f9ffd4efd9c
WilliamFelipe34 <williamfelipe34@gmail.com>
2016-10-29 18:52:15 -0200

    varias alterações feitas

---
+++ foodtruck/Controllers/Avaliação_ComLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class Avaliação_ComLoginController : Controller
+    {
+        // GET: Avaliação_ComLogin
+        public ActionResult AvailarFT_ComLogin()
+        {
+            return View();
+        }
+    }
+}
